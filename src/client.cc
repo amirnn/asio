820e16d187eb7f6737c22486c32f26ccc365bf63
Amir Nourinia <amir.nourinia@gmail.com>
2021-12-24 20:15:07 +0100

    added user options parser

--- src/client.cc
+++ src/client.cc
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <string>
-
-// #define ASIO_STANDALONE
+using std::string;
 #include <boost/asio.hpp>
 #include <boost/asio/ts/buffer.hpp>
 #include <boost/asio/ts/internet.hpp>
-
 #include <boost/json.hpp>
+#include <boost/program_options.hpp>
 
 using namespace boost;
+namespace po = boost::program_options;
 
 // ip address of example.com 93.184.216.34
-std::string ipAddressString = "93.184.216.34";
-unsigned short port = 80;
+string default_ip = "93.184.216.34";
+ushort default_port = 80;
 
-int main(int , char const **)
-{
-    boost::system::error_code ec;
-    asio::ip::address ip = asio::ip::address::from_string(ipAddressString, ec);
-    if (ec.value() != 0)
-    {
-        std::cout 
-            << "Failed to parse the IP address. Error code = "
-            << ec.value() << ". Message: " << ec.message();
-        return ec.value();
-    }
-    
-    asio::ip::tcp::endpoint endpoint(ip, port);
-    
+int main(int ac, char const** av) {
+  po::options_description desc("Allowed options");
+  {
+    desc.add_options()("help", "produce help message")(
+        "ip", po::value<std::string>()->default_value(default_ip), "ip address")(
+        "port", po::value<ushort>()->default_value(default_port), "port number");
+  }
 
+  po::variables_map vm;
+  po::store(po::parse_command_line(ac, av, desc), vm);
+  po::notify(vm);
 
-    return EXIT_SUCCESS;
+  if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+  ushort port = vm.at("port").as<ushort>();
+  string ip_string = vm.at("ip").as<string>();
+
+  boost::system::error_code ec;
+  asio::ip::address ip = asio::ip::address::from_string(ip_string, ec);
+  if (ec.value() != 0) {
+    std::cout << "Failed to parse the IP address. Error code = " << ec.value()
+              << ". Message: " << ec.message();
+    return ec.value();
+  }
+
+  asio::ip::tcp::endpoint endpoint(ip, port);
+
+  return EXIT_SUCCESS;
 }
--- src/server.cc
+++ src/server.cc
@@ -3,9 +3,15 @@
 #include <boost/asio.hpp>
 #include <boost/json.hpp>
 
+using namespace boost;
+
+
+ushort port = 2500;
+
 
 int main(int, char const **)
 {
-    /* code */
+    asio::ip::address ip = asio::ip::address_v4::any();
+    asio::ip::tcp::endpoint endpoint(ip,port);
     return 0;
 }
