c12c06745a0d610836402b17365dbe46a6656362
Amir Nourinia <amir.nourinia@gmail.com>
2021-12-24 15:36:00 +0100

    first init

---
+++ src/asio.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// #define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+
+int main(int, char* []) {
+    std::cout << "Hello, world!\n";
+}
